420c0fe96afb45415c7a34e9c9dc5452cd269ef5
Jean Epp <jean.epp@epitech.eu>
2021-12-08 18:43:06 -0500

    Updating tests

--- TestProject2/Authentification.cs
+++ TestProject2/Authentification.cs
@@ -8,18 +8,12 @@ namespace TestProject2
     class Authentification_Sélénium
     {
         String test_url = "http://localhost:57270/";
-        IWebDriver driver;
-
-        [SetUp]
-        public void start_Browser()
-        {
-            driver = new ChromeDriver(@"C:\Program Files\Google\Chrome\Application");
-            driver.Manage().Window.Maximize();
-        }
+        IWebDriver driver = new ChromeDriver(@"C:\Program Files\Google\Chrome\Application");
 
         [Test, Order(1)]
         public void test_register()
         {
+            driver.Manage().Window.Maximize();
             driver.Url = test_url;
             System.Threading.Thread.Sleep(2000);
             IWebElement searchLink = driver.FindElement(By.XPath("//*[@id='navbarSupportedContent']/div/div[3]/ul[2]/li[3]/a"));
@@ -46,15 +40,12 @@ namespace TestProject2
         [Test, Order(2)]
         public void test_login()
         {
-            driver.Url = test_url;
             System.Threading.Thread.Sleep(2000);
-            IWebElement searchLink = driver.FindElement(By.XPath("//*[@id='navbarSupportedContent']/div/div[3]/ul[2]/li[3]/a"));
-            searchLink.Click();
             IWebElement Email = driver.FindElement(By.Id("Email"));
             Email.SendKeys("jeanepp99@gmail.com");
             IWebElement Password = driver.FindElement(By.Id("Password"));
             Password.SendKeys("1234");
-            searchLink = driver.FindElement(By.XPath("/html/body/div/div/div/form/button"));
+            IWebElement searchLink = driver.FindElement(By.XPath("/html/body/div/div/div/form/button"));
             searchLink.Click();
             System.Threading.Thread.Sleep(2000);
             driver.FindElement(By.XPath("//*[@id='searchStr']"));
@@ -64,19 +55,9 @@ namespace TestProject2
         [Test, Order(3)]
         public void test_logout()
         {
-            driver.Url = test_url;
             IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
             System.Threading.Thread.Sleep(2000);
             IWebElement searchLink = driver.FindElement(By.XPath("//*[@id='navbarSupportedContent']/div/div[3]/ul[2]/li[3]/a"));
-            searchLink.Click();
-            IWebElement Email = driver.FindElement(By.Id("Email"));
-            Email.SendKeys("jeanepp99@gmail.com");
-            IWebElement Password = driver.FindElement(By.Id("Password"));
-            Password.SendKeys("1234");
-            searchLink = driver.FindElement(By.XPath("/html/body/div/div/div/form/button"));
-            searchLink.Click();
-            System.Threading.Thread.Sleep(2000);
-            searchLink = driver.FindElement(By.XPath("//*[@id='navbarSupportedContent']/div/div[3]/ul[2]/li[3]/a"));
             searchLink.Click();
             System.Threading.Thread.Sleep(2000);
             searchLink = driver.FindElement(By.XPath("/html/body/div/div/div/div/button"));
@@ -87,11 +68,6 @@ namespace TestProject2
             System.Threading.Thread.Sleep(2000);
             driver.FindElement(By.XPath("/html/body/div/div/div/form"));
             Console.WriteLine("Test Logout Passed");
-        }
-
-        [TearDown]
-        public void close_Browser()
-        {
             driver.Quit();
         }
     }
